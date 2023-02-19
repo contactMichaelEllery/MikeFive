72a660d7a8d2356c1dfc94dc7a911b02918e6a2b
michaelellery <125519910+michaelellery@users.noreply.github.com>
2023-02-19 16:29:06 -0500

    Second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Hello world from Michael!");
+        System.out.println("Hello world from Michael 33!");
         log.error("Hello World Logger from Michael");
     }
 
